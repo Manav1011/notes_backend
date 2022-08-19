0d106f06fad7f8ef5e84f186f1edce1570d97a1c
manavshah <manavshah1011.ms@gmail.com>
2022-08-19 20:02:52 +0530

    Indian Time Zone

--- notes_backend/settings.py
+++ notes_backend/settings.py
@@ -129,11 +129,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = False
 
 # Email Configurations
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
