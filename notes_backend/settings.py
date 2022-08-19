33d355aee3d3bbc422b1a8fa8d96375cdb81c6dc
manavshah <manavshah1011.ms@gmail.com>
2022-08-19 16:51:11 +0530

    None

--- notes_backend/settings.py
+++ notes_backend/settings.py
@@ -141,11 +141,6 @@ EMAIL_HOST_PASSWORD = 'pxapqaejuyniypow'
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
-import django_heroku
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
