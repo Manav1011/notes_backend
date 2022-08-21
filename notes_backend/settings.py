f6ef3800f223a4ff692fef60eaa2c5283bd1fd7e
manavshah <manavshah1011.ms@gmail.com>
2022-08-21 12:39:14 +0530

    .env push

--- notes_backend/settings.py
+++ notes_backend/settings.py
@@ -12,16 +12,26 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+envvar=BASE_DIR.joinpath('.env')
+
+if os.path.isfile(envvar):
+    dotenv.load_dotenv(envvar)
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i^60*bt1xzkoc)2_g6vyd!hqanrlf2wzqk3pxtnq5bhzs*b5hq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -141,10 +151,10 @@ USE_TZ = False
 # Email Configurations
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'reactdjangopersonalnotes@gmail.com'
-EMAIL_HOST_PASSWORD = 'mghkddkbdirknric'
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
