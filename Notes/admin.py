1521228644d3d4baaf384dab2a6052bae48f0ec0
manavshah <manavshah1011.ms@gmail.com>
2022-08-23 23:57:25 +0530

    date time admin

--- Notes/admin.py
+++ Notes/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import UserNotes
 # Register your models here.
 
-admin.site.register(UserNotes)
+class NotesAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+
+admin.site.register(UserNotes,NotesAdmin)
