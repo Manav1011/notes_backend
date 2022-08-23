b24e1fdc1719fe2503fe784bea61cc7077b42ae3
manavshah <manavshah1011.ms@gmail.com>
2022-08-24 01:08:16 +0530

    Migrations

--- Notes/models.py
+++ Notes/models.py
@@ -11,8 +11,8 @@ class UserNotes(models.Model):
     title=models.CharField(max_length=255,default='No Title')
     content=models.TextField(blank=True,null=True)
     user=models.ForeignKey(UserEmail, related_name='user_notes',on_delete=models.CASCADE)
-    created=models.DateField(auto_now_add=True)
-    updated=models.DateField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return str(self.title)
