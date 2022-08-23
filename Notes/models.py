d032276f41421d2df19f838bd20f945b5ffc2907
manavshah <manavshah1011.ms@gmail.com>
2022-08-24 00:42:00 +0530

    Migrations

--- Notes/models.py
+++ Notes/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from accounts.models import UserEmail
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-import datetime
 
 
 # Create your models here.
@@ -11,15 +10,12 @@ import datetime
 class UserNotes(models.Model):
     title=models.CharField(max_length=255,default='No Title')
     content=models.TextField(blank=True,null=True)
-    user=models.ForeignKey(UserEmail, related_name='user_notes',on_delete=models.CASCADE)    
-    updated=models.CharField(max_length=255)
+    user=models.ForeignKey(UserEmail, related_name='user_notes',on_delete=models.CASCADE)
+    created=models.DateField()
+    updated=models.DateField()
     
     def __str__(self):
         return str(self.title)
     
-    
-@receiver(pre_save,sender=UserNotes)
-def pre_save_receiver(sender,instance,*args,**kwargs):
-    instance.updated = str(datetime.datetime.now()) 
     
     
