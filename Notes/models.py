9c213a73a468324b9eebf4d8265b31988a7e842f
manavshah <manavshah1011.ms@gmail.com>
2022-08-24 00:11:44 +0530

    Delete migrations

--- Notes/models.py
+++ Notes/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from accounts.models import UserEmail
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+import datetime
+
 
 # Create your models here.
 
@@ -8,10 +12,15 @@ class UserNotes(models.Model):
     title=models.CharField(max_length=255,default='No Title')
     content=models.TextField(blank=True,null=True)
     user=models.ForeignKey(UserEmail, related_name='user_notes',on_delete=models.CASCADE)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
+    created=models.DateField(auto_now_add=True)
+    updated=models.CharField(max_length=255)
     
     def __str__(self):
         return str(self.title)
+    
+    
+@receiver(pre_save,sender=UserNotes)
+def pre_save_receiver(sender,instance,*args,**kwargs):
+    instance.updated = str(datetime.datetime.now())    
     
     
