b609d917cfaaf55a549342c56571a815ef438efd
manav <manavshah1011.ms@gmail.com>
2022-11-08 15:03:05 +0530

    update

--- webhooks/views.py
+++ webhooks/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def webhooksview(request):    
     print(request.GET)
     print(request.POST)
