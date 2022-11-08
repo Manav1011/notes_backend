fa31a3aafbaab28df799d2bacf613a8f5849394c
manav <manavshah1011.ms@gmail.com>
2022-11-08 15:09:53 +0530

    update

--- webhooks/views.py
+++ webhooks/views.py
@@ -7,6 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def webhooksview(request):    
-    print(request.GET)
-    print(request.POST)
-    return HttpResponse(request.GET['hub.challenge'])
+    if request.method == 'GET':
+        return HttpResponse(request.GET['hub.challenge'])
+    else:
+        return HttpResponse(request.POST)    
